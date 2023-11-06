db397375cb2e6ff53f87ad43a0d1a64d0bf2d72c
SilverPung <kristofer.pawel@gmail.com>
2023-11-06 14:14:07 +0100

    todo

--- frontend.py
+++ frontend.py
@@ -1,34 +1,46 @@
 import tkinter as tk
 import subprocess
+class Commands:
+    def add_income(self):
+        entry = tk.Entry(window)
+        entry.pack()
 
+        get_button = tk.Button(window, text="Get Text", command=get_text)
+        get_button.pack()
+    def add_expense(self):
+        pass
+    def delete(self):
+        pass
+    def list_items(self):
+        pass
+    def stats(self):
+        pass
 def get_text():
     command = entry.get()
     result_label.config(text=f"You entered: {command}")
-    try:
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    apc=Commands()
+    match(command):
+        case 'delete':
+            apc.delete()
+        case 'add_expense':
+            apc.add_expense()
+        case 'add_income':
+            apc.add_income()
+        case 'list':
+            apc.list()
+        case 'stats':
+            apc.stats()
+if __name__=='__main__':
+    window = tk.Tk()
+    window.title("Get Information Example")
 
-        # Check the return code to see if the command was successful
-        if result.returncode == 0:
-            print("Command executed successfully")
-            print("Standard Output:")
-            print(result.stdout)
-        else:
-            print("Command failed with an error code:", result.returncode)
-            print("Error Output:")
-            print(result.stderr)
-    except Exception as e:
-        print("An error occurred:", str(e))
+    entry = tk.Entry(window)
+    entry.pack()
 
-window = tk.Tk()
-window.title("Get Information Example")
+    get_button = tk.Button(window, text="Get Text", command=get_text)
+    get_button.pack()
 
-entry = tk.Entry(window)
-entry.pack()
+    result_label = tk.Label(window, text="")
+    result_label.pack()
 
-get_button = tk.Button(window, text="Get Text", command=get_text)
-get_button.pack()
-
-result_label = tk.Label(window, text="")
-result_label.pack()
-
-window.mainloop()
+    window.mainloop()
